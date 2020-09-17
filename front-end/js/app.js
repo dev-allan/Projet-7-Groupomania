f10707349b61310ec5ddc96f95a917cfd95d11a2
Allan <allanduminy@protonmail.com>
2020-09-17 23:02:13 +0200

    page connect.html terminée

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -1,27 +1,11 @@
 let vm = new Vue({
     el: "#authentification",
     data:{
-        test: true,
+        formulaire__inscription: false,
+        formulaire__authentification : false,
     },
     methods: {
-        formulaire_inscription: () => {
-            let success = 0;
-            success = +1;
-            console.log(success);
-            document.querySelector("form").innerHTML+=`<div class="form-group">
-            <label for="exampleInputEmail1">Votre email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="azerty@mail.com" placeholder="azerty@mail.com">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Votre mot de passe</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Votre mot de passe">
-            <label for="exampleInputPassword1">Retappez votre mot de passe</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Votre mot de passe">
-          </div>
-          <button type="submit" class="btn btn-dark">Créer votre compte</button>`;
-          success = +1;
-          console.log(success)
-        },
+        
         formulaire_authentification: () => {
             document.querySelector('form').innerHTML+=`<div class="form-group">
             <label for="exampleInputEmail1">Votre email</label>
