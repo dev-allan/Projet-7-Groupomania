329761ab7c9e191ff9cd3bf274f1f07461cdf576
Allan <allanduminy@protonmail.com>
2020-09-30 19:16:28 +0200

    création de la page login

--- front-end/src/views/Login.vue
+++ front-end/src/views/Login.vue
@@ -1,37 +1,42 @@
 <template>
     <div class="login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
+        <h1>Se connecter</h1>
+        <form @submit.prevent="submitLogin">
+            <input type="email" name="email" placeholder="Email" v-model="email"/>
+            <input type="password" name="password"  placeholder="Password" v-model="password" />
+            <button type="submit">Login</button>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Login',
-        data() {
+        data(){
             return {
-                input: {
-                    username: "",
-                    password: ""
-                }
+                email: '',
+                password : '',
             }
         },
         methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "secure" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
+            submitLogin() {
+                const user = {
+                    email : this.email,
+                    password : this.password
                 }
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(user), 
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                fetch('http://localhost:3000/utilisateurs/login', options)
+                    .then(res => res.json(),)
+                    .then(res => localStorage.setItem('token', res.token),
+                        window.location.href = "/")
             }
         }
+
     }
 </script>
 
