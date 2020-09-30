80754445f87db9baf014fb98fa02e99a0d89127d
Allan <allanduminy@protonmail.com>
2020-09-30 15:29:07 +0200

    mise en place des routes de navigation front end

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Accueil</router-link> |
+      <router-link to="/about">Publier</router-link> |
+      <router-link to="/profil">Profil</router-link> |
+      <router-link to="/login">Se connecter</router-link> |
+      <router-link to="">Se déconnecter</router-link> |
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+	name: 'App',
+	components: {
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -17,7 +17,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path : '/login',
+    name: 'Login',
+    component: () => import(/*login*/ '../views/Login.vue')
+  },
+  {
+    path : '/profil',
+    name: 'Profil',
+    component: () => import(/*Profil user*/ '../views/Profil.vue')
+  },
 ]
 
 const router = new VueRouter({
